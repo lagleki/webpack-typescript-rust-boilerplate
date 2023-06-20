31399fe5426a806c8e191aa6bd6b05ea71a74b94
lagleki <gleki.is.my.name@gmail.com>
2023-06-20 12:31:48 +0200

    1.0.35

--- src/ts/front.ts
+++ src/ts/front.ts
@@ -1,19 +1,51 @@
 import { component, store } from "../../../renderer/src";
+// import io from 'socket.io-client'
+// import arrowCreate, { HEAD } from 'arrows-svg'
+// import {
+//   listFamymaho,
+//   modes,
+//   supportedLangs,
+//   initState,
+//   initStateLoading,
+// } from './worker/template/utils/consts'
+// import { rows } from './worker/template/utils/pronunciation.js'
+// import { UNICODE_START, lerfu_index } from './worker/template/utils/zlm.js'
 
-const state = store({ loading: true, x: "john", y: "mary" });
-//simple console logging test
-const log = (arg: { [key: string]: string }) => console.log(arg);
+// //global vars:
+// let resultCount
+// let typingTimer //search after timeout
+// let cacheObj
+// let scrollTimer = null
+// let scrollJvoTimer = null
+// const timers = {
+//   vh: null,
+//   typing: null,
+// }
+// let socket, socket1Chat, socket1Chat_connected
 
-log({ event: "Logging into console", status: "ok" });
+// //stores
+// const stateLeijufra = store({ custom_links: [] })
+// let state = store(initState)
+// const loadingState = store(initStateLoading)
+
+const storeOptions = { localCacheKey: "test", eventKey: 'test2' }
+const state = store(
+  { loading: true, sub: { x: "john", y: "mary" } },
+  storeOptions
+);
+// //simple console logging test
+// const log = (arg: { [key: string]: string }) => console.log(arg);
+
+// log({ event: "Logging into console", status: "ok" });
 
 component("#root", () => {
   const div = document.createElement("div");
-  div.textContent = `Hello, ${state.x} !`;
+  div.textContent = `Hello, ${state.sub.x} !`;
   return div;
-});
+}, storeOptions);
 
 setTimeout(() => {
-  state.x = "maarr";
+  state.sub.x = "maarr";
 }, 3000);
 
 //HTML templating engine test
