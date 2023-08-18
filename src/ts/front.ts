6b4d464b07ce467213c1baf28fda396c4dbb1b33
lagleki <gleki.is.my.name@gmail.com>
2023-08-18 12:33:29 +0200

    fix

--- src/ts/front.ts
+++ src/ts/front.ts
@@ -2,30 +2,12 @@ import { component, store, h } from "../../../renderer/src";
 import { rows } from "./utils/pronunciation";
 import tejufra from "./worker/template/tejufra.json";
 
-import { mathjax } from 'mathjax-full/js/mathjax'
-import { TeX } from 'mathjax-full/js/input/tex'
-import { SVG } from 'mathjax-full/js/output/svg'
-import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages'
-import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor'
-import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html'
-
-const adaptor = liteAdaptor()
-RegisterHTMLHandler(adaptor)
-
-const mathjax_document = mathjax.document('', {
-  InputJax: new TeX({ packages: AllPackages }),
-  OutputJax: new SVG({ fontCache: 'local' })
-})
-
-const mathjax_options = {
-  em: 16,
-  ex: 8,
-  containerWidth: 1280
-}
+import temml from "./libs/Temml/temml";
 
 export function get_mathjax_svg(math: string): string {
-  const node = mathjax_document.convert(math, mathjax_options)
-  return adaptor.innerHTML(node)
+  const tex = "c = \\pm\\sqrt{a^2 + b^2}";
+  const temmlOptions = {};
+  return temml.renderToString(tex, temmlOptions);
 }
 
 // import io from "socket.io-client";
